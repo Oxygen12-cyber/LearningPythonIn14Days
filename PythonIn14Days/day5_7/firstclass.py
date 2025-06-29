847677232e65e53ade52088eff5bc8f8453bf036
Oxygen12-cyber <adeolaodunjo32@gmail.com>
2025-06-29 14:43:54 +0100

    added new folder day5_7

---
+++ PythonIn14Days/day5_7/firstclass.py
@@ -0,0 +1,11 @@
+def main():
+    class Dog():
+        def __init__(self, name, type):
+            self.name = name
+            self.type = type
+
+        def bark():
+            pass
+
+if __name__ == "__main__":
+    main()
