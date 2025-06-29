3853b07467744e295664a61a6218c1af536ddf7f
Adeola Odunjo <86977028+Oxygen12-cyber@users.noreply.github.com>
2025-06-29 14:09:20 +0000

    Refactor Dog class instantiation and add print statement for dog details

--- PythonIn14Days/day5_7/firstclass.py
+++ PythonIn14Days/day5_7/firstclass.py
@@ -4,8 +4,14 @@ def main():
             self.name = name
             self.type = type
 
-        def bark():
-            pass
+    dog1 = Dog()
+    dog2 = Dog()
+    dog1.name = 'Fredric'
+    dog2.name = 'jamie'
+    dog1.type = 'labrador'
+    dog2.type = 'collie'
+
+    print(f'my moms dog is {dog1.name} and my dog is {dog2.name} \n mine is a {dog2.type} and my mom is a{dog1.type}')
 
 if __name__ == "__main__":
     main()
